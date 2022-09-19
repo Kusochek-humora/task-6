ab800cf21a55ab8c964fd44f8c4d5e7c8b677518
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2022-09-19 19:01:13 +0600

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,15 @@
 const percent = 0.2,
-  checkbox = document.querySelector("#switcher");
-let prices = document.querySelectorAll(".pricing-card__value");
+  checkbox = document.querySelector("#switcher"),
+  prices = document.querySelectorAll(".pricing-card__value");
 checkbox.addEventListener("change", (e) => {
-  //   let pricesArray = prices.push();
-  //   console.log(pricesArray);
-
   if (checkbox.checked === !true) {
     prices.forEach((item, index) => {
-      item.innerHTML =  Math.round(item.innerHTML * 12 - percent * (item.innerHTML * 12));
+      item.innerHTML = Math.round(
+        item.innerHTML * 12 - percent * (item.innerHTML * 12)
+      );
     });
   } else {
     prices.forEach((item, index) => {
-      console.log(item.innerHTML / (12 * (1 - percent)));
       item.innerHTML = Math.round(item.innerHTML / (12 * (1 - percent)));
     });
   }
